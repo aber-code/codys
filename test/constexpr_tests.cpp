610bf1f231f91ce6bd8aa7fa540f2a1862d704f8
Arne Berger <s_4g5rpo@uni-bremen.de>
2022-02-16 16:24:02 +0100

    Add constexpr tests for State

--- test/constexpr_tests.cpp
+++ test/constexpr_tests.cpp
@@ -6,9 +6,13 @@
 #include <units/isq/si/speed.h>
 #include <units/isq/si/length.h>
 
+#include <boost/hana/tuple.hpp>
+
 #include <codys.hpp>
 
-using Position = State<class Position_, units::isq::si::length<units::isq::si::metre>>;
+using PositionTag = class Position_;
+using PositionUnit = units::isq::si::length<units::isq::si::metre>;
+using Position = State<PositionTag, PositionUnit>;
 using Velocity = State<class Vel_, units::isq::si::speed<units::isq::si::metre_per_second>>;
 
 using BasicMotions = System<Position, Velocity>;
@@ -19,3 +23,17 @@ TEST_CASE("System return right state indices", "[System]")
   STATIC_REQUIRE(BasicMotions::idx_of<Velocity>() == 1);
 }
 
+TEST_CASE("State gives self as depends_on", "[State]")
+{
+  STATIC_REQUIRE(std::is_same_v<Position::depends_on, boost::hana::tuple<Position>>);
+}
+
+TEST_CASE("State has right unit as type", "[State]")
+{
+  STATIC_REQUIRE(std::is_same_v<Position::Unit, PositionUnit>);
+}
+
+TEST_CASE("State has right tag as type", "[State]")
+{
+  STATIC_REQUIRE(std::is_same_v<Position::Tag, PositionTag>);
+}
