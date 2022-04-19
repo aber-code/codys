eb6ac3a2ff2864114e3ec38b715fb78a0644dcc8
aber-code <aber.code.github@gmail.com>
2022-04-19 09:15:39 +0200

    Add basic operators for physical values

--- include/codys/Operators.hpp
+++ include/codys/Operators.hpp
@@ -30,4 +30,67 @@ constexpr auto operator+([[maybe_unused]] State<Lhs, Unit> lhs,
     return Plus<State<Lhs, Unit>, State<Rhs, Unit>, Unit>{};
 }
 
+template <class Lhs, class Rhs, class Unit_>
+struct Minus {
+    using depends_on =
+        decltype(boost::hana::concat(std::declval<typename Lhs::depends_on>(),
+                                     std::declval<typename Rhs::depends_on>()));
+
+    using Unit = Unit_;
+
+    template <class SystemType, std::size_t N> 
+    constexpr static double evaluate(std::span<const double, N> arr) {
+        return Lhs::template evaluate<SystemType>(arr) -
+               Rhs::template evaluate<SystemType>(arr);
+    }
+};
+
+template <class Lhs, class Rhs, class Unit> 
+constexpr auto operator-([[maybe_unused]] State<Lhs, Unit> lhs,
+                         [[maybe_unused]] State<Rhs, Unit> rhs) {
+    return Minus<State<Lhs, Unit>, State<Rhs, Unit>, Unit>{};
+}
+
+template <class Lhs, class Rhs, class UnitLhs, class UnitRhs>
+struct Multiply {
+    using depends_on =
+        decltype(boost::hana::concat(std::declval<typename Lhs::depends_on>(),
+                                     std::declval<typename Rhs::depends_on>()));
+
+    using Unit = decltype(std::declval<UnitLhs>() * std::declval<UnitRhs>());
+
+    template <class SystemType, std::size_t N> 
+    constexpr static double evaluate(std::span<const double, N> arr) {
+        return Lhs::template evaluate<SystemType>(arr) *
+               Rhs::template evaluate<SystemType>(arr);
+    }
+};
+
+template <class Lhs, class Rhs, class UnitLhs, class UnitRhs> 
+constexpr auto operator*([[maybe_unused]] State<Lhs, UnitLhs> lhs,
+                         [[maybe_unused]] State<Rhs, UnitRhs> rhs) {
+    return Multiply<State<Lhs, UnitLhs>, State<Rhs, UnitRhs>, UnitLhs, UnitRhs>{};
+}
+
+template <class Lhs, class Rhs, class UnitLhs, class UnitRhs>
+struct Divide {
+    using depends_on =
+        decltype(boost::hana::concat(std::declval<typename Lhs::depends_on>(),
+                                     std::declval<typename Rhs::depends_on>()));
+
+    using Unit = decltype(std::declval<UnitLhs>() / std::declval<UnitRhs>());
+
+    template <class SystemType, std::size_t N> 
+    constexpr static double evaluate(std::span<const double, N> arr) {
+        return Lhs::template evaluate<SystemType>(arr) /
+               Rhs::template evaluate<SystemType>(arr);
+    }
+};
+
+template <class Lhs, class Rhs, class UnitLhs, class UnitRhs> 
+constexpr auto operator/([[maybe_unused]] State<Lhs, UnitLhs> lhs,
+                         [[maybe_unused]] State<Rhs, UnitRhs> rhs) {
+    return Divide<State<Lhs, UnitLhs>, State<Rhs, UnitRhs>, UnitLhs, UnitRhs>{};
+}
+
 } // namespace codys
--- test/constexpr_tests.cpp
+++ test/constexpr_tests.cpp
@@ -4,6 +4,8 @@
 
 #include <units/isq/si/speed.h>
 #include <units/isq/si/length.h>
+#include <units/isq/si/area.h>
+#include <units/generic/dimensionless.h>
 
 #include <boost/hana/tuple.hpp>
 
@@ -11,6 +13,8 @@
 
 using PositionTag = class Position_;
 using PositionUnit = units::isq::si::length<units::isq::si::metre>;
+using PositionUnitSq = units::isq::si::area<units::isq::si::square_metre>;
+using Dimensionless = units::dimensionless<units::one>;
 using Position = codys::State<PositionTag, PositionUnit>;
 using Velocity = codys::State<class Vel_, units::isq::si::speed<units::isq::si::metre_per_second>>;
 
@@ -38,6 +42,16 @@ TEST_CASE("Derivative has right operand as type", "[Derivative]")
   STATIC_REQUIRE(std::is_same_v<decltype(derivative)::Operand, Position>);
 }
 
+TEST_CASE("Operator Plus yields sum of operands", "[Operator]") 
+{
+  static constexpr auto plus = Position{} + Position{};
+  static constexpr std::array values{1.0};
+  using TestSystem = codys::System<Position>;
+
+  static constexpr auto result = plus.template evaluate<TestSystem, 1>(values);
+  STATIC_REQUIRE(result == values[0] + values[0]);
+}
+
 TEST_CASE("Operator Plus has same unit as operands", "[Operator]")
 {
   static constexpr auto plus = Position{} + Position{};
@@ -49,4 +63,73 @@ TEST_CASE("Operator Plus has concatination of denepends on", "[Operator]")
   static constexpr auto plus = Position{} + Position{};
   static constexpr auto ref_dependands = boost::hana::tuple<Position,Position>();
   STATIC_REQUIRE(std::is_same_v<decltype(plus)::depends_on, std::remove_cvref_t<decltype(ref_dependands)>>);
+}
+
+TEST_CASE("Operator Minus yields substraction of operands", "[Operator]") 
+{
+  static constexpr auto minus = Position{} - Position{};
+  static constexpr std::array values{1.0};
+  using TestSystem = codys::System<Position>;
+
+  static constexpr auto result = minus.template evaluate<TestSystem, 1>(values);
+  STATIC_REQUIRE(result == values[0] - values[0]);
+}
+
+TEST_CASE("Operator Minus has same unit as operands", "[Operator]")
+{
+  static constexpr auto minus = Position{} - Position{};
+  STATIC_REQUIRE(std::is_same_v<decltype(minus)::Unit, PositionUnit>);
+}
+
+TEST_CASE("Operator Minus has concatination of denepends on", "[Operator]")
+{
+  static constexpr auto minus = Position{} - Position{};
+  static constexpr auto ref_dependands = boost::hana::tuple<Position,Position>();
+  STATIC_REQUIRE(std::is_same_v<decltype(minus)::depends_on, std::remove_cvref_t<decltype(ref_dependands)>>);
+}
+
+TEST_CASE("Operator Multiply yields multiplication of operands", "[Operator]") 
+{
+  static constexpr auto multiply = Position{} * Position{};
+  static constexpr std::array values{2.0};
+  using TestSystem = codys::System<Position>;
+
+  static constexpr auto result = multiply.template evaluate<TestSystem, 1>(values);
+  STATIC_REQUIRE(result == values[0] * values[0]);
+}
+
+TEST_CASE("Operator Multiply has unit resulting from multiplying operands", "[Operator]")
+{
+  static constexpr auto multiply = Position{} * Position{};
+  STATIC_REQUIRE(std::is_same_v<decltype(multiply)::Unit, PositionUnitSq>);
+}
+
+TEST_CASE("Operator Multiply has concatination of denepends on", "[Operator]")
+{
+  static constexpr auto multiply = Position{} * Position{};
+  static constexpr auto ref_dependands = boost::hana::tuple<Position,Position>();
+  STATIC_REQUIRE(std::is_same_v<decltype(multiply)::depends_on, std::remove_cvref_t<decltype(ref_dependands)>>);
+}
+
+TEST_CASE("Operator Divide yields multiplication of operands", "[Operator]") 
+{
+  static constexpr auto divide = Position{} / Position{};
+  static constexpr std::array values{2.0};
+  using TestSystem = codys::System<Position>;
+
+  static constexpr auto result = divide.template evaluate<TestSystem, 1>(values);
+  STATIC_REQUIRE(result == values[0] / values[0]);
+}
+
+TEST_CASE("Operator Divide has unit resulting from dividing operands", "[Operator]")
+{
+  static constexpr auto divide = Position{} / Position{};
+  STATIC_REQUIRE(std::is_same_v<decltype(divide)::Unit, Dimensionless>);
+}
+
+TEST_CASE("Operator Divide has concatination of denepends on", "[Operator]")
+{
+  static constexpr auto divide = Position{} / Position{};
+  static constexpr auto ref_dependands = boost::hana::tuple<Position,Position>();
+  STATIC_REQUIRE(std::is_same_v<decltype(divide)::depends_on, std::remove_cvref_t<decltype(ref_dependands)>>);
 }
