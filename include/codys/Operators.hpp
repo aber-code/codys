a6f1f61f76e50ce9339ac4d0813c89b99ac2b0fd
aber-code <aber.code.github@gmail.com>
2022-04-20 09:28:12 +0200

    Add basic operator with scalar valus

--- include/codys/Operators.hpp
+++ include/codys/Operators.hpp
@@ -10,45 +10,115 @@
 namespace codys {
 
 template <class Lhs, class Rhs, class Unit_>
-struct Plus {
+struct Add {
     using depends_on =
         decltype(boost::hana::concat(std::declval<typename Lhs::depends_on>(),
                                      std::declval<typename Rhs::depends_on>()));
-
     using Unit = Unit_;
 
+    Lhs lhs;
+    Rhs rhs;
+
     template <class SystemType, std::size_t N> 
-    constexpr static double evaluate(std::span<const double, N> arr) {
-        return Lhs::template evaluate<SystemType>(arr) +
-               Rhs::template evaluate<SystemType>(arr);
+    constexpr double evaluate(std::span<const double, N> arr) const {
+        return lhs.template evaluate<SystemType>(arr) +
+               rhs.template evaluate<SystemType>(arr);
     }
 };
 
 template <class Lhs, class Rhs, class Unit> 
 constexpr auto operator+([[maybe_unused]] State<Lhs, Unit> lhs,
                          [[maybe_unused]] State<Rhs, Unit> rhs) {
-    return Plus<State<Lhs, Unit>, State<Rhs, Unit>, Unit>{};
+    return Add<State<Lhs, Unit>, State<Rhs, Unit>, Unit>{lhs, rhs};
+}
+
+template <PhysicalValue Scalar, class Rhs, class UnitRhs>
+struct AddScalar {
+    using depends_on = typename Rhs::depends_on;
+    using Unit = decltype(std::declval<Scalar>() + std::declval<UnitRhs>());
+
+    Scalar lhs;
+    Rhs rhs;
+
+    template <class SystemType, std::size_t N> 
+    constexpr double evaluate(std::span<const double, N> arr) const {
+        return lhs.number() + rhs.template evaluate<SystemType>(arr);
+    }
+};
+
+template <PhysicalValue Scalar, class Rhs, class UnitRhs> 
+constexpr auto operator+([[maybe_unused]] Scalar lhs,
+                         [[maybe_unused]] State<Rhs, UnitRhs> rhs) {
+    return AddScalar<Scalar, State<Rhs, UnitRhs>, UnitRhs>{lhs, rhs};
+}
+
+template <PhysicalValue Scalar, class Lhs, class UnitLhs> 
+constexpr auto operator+([[maybe_unused]] State<Lhs, UnitLhs> lhs,
+                         [[maybe_unused]] Scalar rhs) {
+    return AddScalar<Scalar, State<Lhs, UnitLhs>, UnitLhs>{rhs, lhs};
 }
 
 template <class Lhs, class Rhs, class Unit_>
-struct Minus {
+struct Substract {
     using depends_on =
         decltype(boost::hana::concat(std::declval<typename Lhs::depends_on>(),
                                      std::declval<typename Rhs::depends_on>()));
-
     using Unit = Unit_;
 
+    Lhs lhs;
+    Rhs rhs;
+
     template <class SystemType, std::size_t N> 
-    constexpr static double evaluate(std::span<const double, N> arr) {
-        return Lhs::template evaluate<SystemType>(arr) -
-               Rhs::template evaluate<SystemType>(arr);
+    constexpr double evaluate(std::span<const double, N> arr) const {
+        return lhs.template evaluate<SystemType>(arr) -
+               rhs.template evaluate<SystemType>(arr);
     }
 };
 
 template <class Lhs, class Rhs, class Unit> 
 constexpr auto operator-([[maybe_unused]] State<Lhs, Unit> lhs,
                          [[maybe_unused]] State<Rhs, Unit> rhs) {
-    return Minus<State<Lhs, Unit>, State<Rhs, Unit>, Unit>{};
+    return Substract<State<Lhs, Unit>, State<Rhs, Unit>, Unit>{lhs, rhs};
+}
+
+template <PhysicalValue Scalar, class Rhs, class UnitRhs>
+struct SubstractScalarFromLeft {
+    using depends_on = typename Rhs::depends_on;
+    using Unit = decltype(std::declval<Scalar>() - std::declval<UnitRhs>());
+
+    Scalar lhs;
+    Rhs rhs;
+
+    template <class SystemType, std::size_t N> 
+    constexpr double evaluate(std::span<const double, N> arr) const {
+        return lhs.number() - rhs.template evaluate<SystemType>(arr);
+    }
+};
+
+template <class Lhs, class UnitLhs, PhysicalValue Scalar>
+struct SubstractScalarFromRight {
+    using depends_on = typename Lhs::depends_on;
+    using Unit = decltype(std::declval<UnitLhs>() - std::declval<Scalar>());
+
+    Lhs lhs;
+    Scalar rhs;
+
+    template <class SystemType, std::size_t N> 
+    constexpr double evaluate(std::span<const double, N> arr) const {
+        return lhs.template evaluate<SystemType>(arr) - rhs.number();
+    }
+};
+
+template <PhysicalValue Scalar, class Rhs, class UnitRhs> 
+constexpr auto operator-([[maybe_unused]] Scalar lhs,
+                         [[maybe_unused]] State<Rhs, UnitRhs> rhs) {
+    return SubstractScalarFromLeft<Scalar, State<Rhs, UnitRhs>, UnitRhs>{lhs, rhs};
+}
+
+template <class Lhs, class UnitLhs, PhysicalValue Scalar> 
+constexpr auto operator-([[maybe_unused]] State<Lhs, UnitLhs> lhs,
+                         [[maybe_unused]] Scalar rhs) {
+    return SubstractScalarFromRight<State<Lhs, UnitLhs>, UnitLhs, Scalar>{lhs, rhs};
 }
 
 template <class Lhs, class Rhs, class UnitLhs, class UnitRhs>
@@ -56,20 +126,48 @@ struct Multiply {
     using depends_on =
         decltype(boost::hana::concat(std::declval<typename Lhs::depends_on>(),
                                      std::declval<typename Rhs::depends_on>()));
-
     using Unit = decltype(std::declval<UnitLhs>() * std::declval<UnitRhs>());
 
+    Lhs lhs;
+    Rhs rhs;
+
     template <class SystemType, std::size_t N> 
-    constexpr static double evaluate(std::span<const double, N> arr) {
-        return Lhs::template evaluate<SystemType>(arr) *
-               Rhs::template evaluate<SystemType>(arr);
+    constexpr double evaluate(std::span<const double, N> arr) const {
+        return lhs.template evaluate<SystemType>(arr) *
+               rhs.template evaluate<SystemType>(arr);
     }
 };
 
 template <class Lhs, class Rhs, class UnitLhs, class UnitRhs> 
 constexpr auto operator*([[maybe_unused]] State<Lhs, UnitLhs> lhs,
                          [[maybe_unused]] State<Rhs, UnitRhs> rhs) {
-    return Multiply<State<Lhs, UnitLhs>, State<Rhs, UnitRhs>, UnitLhs, UnitRhs>{};
+    return Multiply<State<Lhs, UnitLhs>, State<Rhs, UnitRhs>, UnitLhs, UnitRhs>{lhs, rhs};
+}
+
+template <PhysicalValue Scalar, class Rhs, class UnitRhs>
+struct MultiplyScalar {
+    using depends_on = typename Rhs::depends_on;
+    using Unit = decltype(std::declval<Scalar>() * std::declval<UnitRhs>());
+
+    Scalar lhs;
+    Rhs rhs;
+
+    template <class SystemType, std::size_t N> 
+    constexpr double evaluate(std::span<const double, N> arr) const {
+        return lhs.number() * rhs.template evaluate<SystemType>(arr);
+    }
+};
+
+template <PhysicalValue Scalar, class Rhs, class UnitRhs> 
+constexpr auto operator*([[maybe_unused]] Scalar lhs,
+                         [[maybe_unused]] State<Rhs, UnitRhs> rhs) {
+    return MultiplyScalar<Scalar, State<Rhs, UnitRhs>, UnitRhs>{lhs, rhs};
+}
+
+template <PhysicalValue Scalar, class Lhs, class UnitLhs> 
+constexpr auto operator*([[maybe_unused]] State<Lhs, UnitLhs> lhs,
+                         [[maybe_unused]] Scalar rhs) {
+    return MultiplyScalar<Scalar, State<Lhs, UnitLhs>, UnitLhs>{rhs, lhs};
 }
 
 template <class Lhs, class Rhs, class UnitLhs, class UnitRhs>
@@ -77,20 +175,62 @@ struct Divide {
     using depends_on =
         decltype(boost::hana::concat(std::declval<typename Lhs::depends_on>(),
                                      std::declval<typename Rhs::depends_on>()));
-
     using Unit = decltype(std::declval<UnitLhs>() / std::declval<UnitRhs>());
 
+    Lhs lhs;
+    Rhs rhs;
+
     template <class SystemType, std::size_t N> 
-    constexpr static double evaluate(std::span<const double, N> arr) {
-        return Lhs::template evaluate<SystemType>(arr) /
-               Rhs::template evaluate<SystemType>(arr);
+    constexpr double evaluate(std::span<const double, N> arr) const {
+        return lhs.template evaluate<SystemType>(arr) /
+               rhs.template evaluate<SystemType>(arr);
     }
 };
 
 template <class Lhs, class Rhs, class UnitLhs, class UnitRhs> 
 constexpr auto operator/([[maybe_unused]] State<Lhs, UnitLhs> lhs,
                          [[maybe_unused]] State<Rhs, UnitRhs> rhs) {
-    return Divide<State<Lhs, UnitLhs>, State<Rhs, UnitRhs>, UnitLhs, UnitRhs>{};
+    return Divide<State<Lhs, UnitLhs>, State<Rhs, UnitRhs>, UnitLhs, UnitRhs>{lhs, rhs};
+}
+
+template <PhysicalValue Scalar, class Rhs, class UnitRhs>
+struct DivideScalarFromLeft {
+    using depends_on = typename Rhs::depends_on;
+    using Unit = decltype(std::declval<Scalar>() / std::declval<UnitRhs>());
+
+    Scalar lhs;
+    Rhs rhs;
+
+    template <class SystemType, std::size_t N> 
+    constexpr double evaluate(std::span<const double, N> arr) const {
+        return lhs.number() / rhs.template evaluate<SystemType>(arr);
+    }
+};
+
+template <class Lhs, class UnitLhs, PhysicalValue Scalar>
+struct DivideScalarFromRight {
+    using depends_on = typename Lhs::depends_on;
+    using Unit = decltype(std::declval<UnitLhs>() / std::declval<Scalar>());
+
+    Lhs lhs;
+    Scalar rhs;
+
+    template <class SystemType, std::size_t N> 
+    constexpr double evaluate(std::span<const double, N> arr) const {
+        return lhs.template evaluate<SystemType>(arr) / rhs.number();
+    }
+};
+
+template <PhysicalValue Scalar, class Rhs, class UnitRhs> 
+constexpr auto operator/([[maybe_unused]] Scalar lhs,
+                         [[maybe_unused]] State<Rhs, UnitRhs> rhs) {
+    return DivideScalarFromLeft<Scalar, State<Rhs, UnitRhs>, UnitRhs>{lhs, rhs};
+}
+
+template <class Lhs, class UnitLhs, PhysicalValue Scalar> 
+constexpr auto operator/([[maybe_unused]] State<Lhs, UnitLhs> lhs,
+                         [[maybe_unused]] Scalar rhs) {
+    return DivideScalarFromRight<State<Lhs, UnitLhs>, UnitLhs, Scalar>{lhs, rhs};
 }
 
 } // namespace codys
--- test/constexpr_tests.cpp
+++ test/constexpr_tests.cpp
@@ -3,6 +3,7 @@
 #include <array>
 
 #include <units/isq/si/speed.h>
+#include <units/isq/si/time.h>
 #include <units/isq/si/length.h>
 #include <units/isq/si/area.h>
 #include <units/generic/dimensionless.h>
@@ -16,7 +17,8 @@ using PositionUnit = units::isq::si::length<units::isq::si::metre>;
 using PositionUnitSq = units::isq::si::area<units::isq::si::square_metre>;
 using Dimensionless = units::dimensionless<units::one>;
 using Position = codys::State<PositionTag, PositionUnit>;
-using Velocity = codys::State<class Vel_, units::isq::si::speed<units::isq::si::metre_per_second>>;
+using VelocityUnit = units::isq::si::speed<units::isq::si::metre_per_second>;
+using Velocity = codys::State<class Vel_, VelocityUnit>;
 
 using BasicMotions = codys::System<Position, Velocity>;
 
@@ -111,7 +113,7 @@ TEST_CASE("Operator Multiply has concatination of denepends on", "[Operator]")
   STATIC_REQUIRE(std::is_same_v<decltype(multiply)::depends_on, std::remove_cvref_t<decltype(ref_dependands)>>);
 }
 
-TEST_CASE("Operator Divide yields multiplication of operands", "[Operator]") 
+TEST_CASE("Operator Divide yields division of operands", "[Operator]") 
 {
   static constexpr auto divide = Position{} / Position{};
   static constexpr std::array values{2.0};
@@ -132,4 +134,212 @@ TEST_CASE("Operator Divide has concatination of denepends on", "[Operator]")
   static constexpr auto divide = Position{} / Position{};
   static constexpr auto ref_dependands = boost::hana::tuple<Position,Position>();
   STATIC_REQUIRE(std::is_same_v<decltype(divide)::depends_on, std::remove_cvref_t<decltype(ref_dependands)>>);
+}
+
+TEST_CASE("Operator Add to Scalar yields sum of operands", "[Operator]") 
+{
+  using namespace units::isq::si::references;
+  static constexpr auto scalar_number = 10.0;
+  static constexpr auto displacement = scalar_number * m;
+  static constexpr auto state = Position{};
+  static constexpr auto scalar_sum = displacement + state;
+  static constexpr std::array values{2.0};
+  using TestSystem = codys::System<Position>;
+
+  static constexpr auto result = scalar_sum.template evaluate<TestSystem, 1>(values);
+  STATIC_REQUIRE(result == scalar_number + values[0]);
+}
+
+TEST_CASE("Operator Add to Scalar is commutative", "[Operator]") 
+{
+  using namespace units::isq::si::references;
+  static constexpr auto scalar_number = 10.0;
+  static constexpr auto displacement = scalar_number * m;
+  static constexpr auto state = Position{};
+  static constexpr auto scalar_sum1 = displacement + state;
+  static constexpr auto scalar_sum2 = state + displacement;
+  static constexpr std::array values{2.0};
+  using TestSystem = codys::System<Position>;
+
+  static constexpr auto result1 = scalar_sum1.template evaluate<TestSystem, 1>(values);
+  static constexpr auto result2 = scalar_sum2.template evaluate<TestSystem, 1>(values);
+  STATIC_REQUIRE(result1 == result2);
+}
+
+TEST_CASE("Operator Add to Scalar does not change unit", "[Operator]")
+{
+  using namespace units::isq::si::references;
+  static constexpr auto displacement = 10 * m;
+  static constexpr auto state = Position{};
+  static constexpr auto scalar_sum = displacement + state;
+  STATIC_REQUIRE(std::is_same_v<decltype(scalar_sum)::Unit, PositionUnit>);
+}
+
+TEST_CASE("Operator Add to Scalar does not change dependencies", "[Operator]")
+{
+  using namespace units::isq::si::references;
+  static constexpr auto displacement = 10 * m;
+  static constexpr auto state = Position{};
+  static constexpr auto scalar_sum = displacement + state;
+  STATIC_REQUIRE(std::is_same_v<decltype(scalar_sum)::depends_on, decltype(state)::depends_on>);
+}
+
+TEST_CASE("Operator Minus with Scalar yields substraction of operands", "[Operator]") 
+{
+  using namespace units::isq::si::references;
+  static constexpr auto scalar_number = 10.0;
+  static constexpr auto displacement = scalar_number * m;
+  static constexpr auto state = Position{};
+  static constexpr auto scalar_substraction = displacement - state;
+  static constexpr std::array values{2.0};
+  using TestSystem = codys::System<Position>;
+
+  static constexpr auto result = scalar_substraction.template evaluate<TestSystem, 1>(values);
+  STATIC_REQUIRE(result == scalar_number - values[0]);
+}
+
+TEST_CASE("Operator Minus with Scalar is commutative", "[Operator]") 
+{
+  using namespace units::isq::si::references;
+  static constexpr auto scalar_number = 10.0;
+  static constexpr auto displacement = scalar_number * m;
+  static constexpr auto state = Position{};
+  static constexpr auto scalar_substraction1 = displacement - state;
+  static constexpr auto scalar_substraction2 = state - displacement;
+  static constexpr std::array values{2.0};
+  using TestSystem = codys::System<Position>;
+
+  static constexpr auto result1 = scalar_substraction1.template evaluate<TestSystem, 1>(values);
+  static constexpr auto result2 = scalar_substraction2.template evaluate<TestSystem, 1>(values);
+  STATIC_REQUIRE(result1 == -result2);
+}
+
+TEST_CASE("Operator Minus with Scalar does not change unit", "[Operator]")
+{
+  using namespace units::isq::si::references;
+  static constexpr auto displacement = 10 * m;
+  static constexpr auto state = Position{};
+  static constexpr auto scalar_substraction = displacement - state;
+  STATIC_REQUIRE(std::is_same_v<decltype(scalar_substraction)::Unit, PositionUnit>);
+}
+
+TEST_CASE("Operator Minus with Scalar does not change dependencies", "[Operator]")
+{
+  using namespace units::isq::si::references;
+  static constexpr auto displacement = 10 * m;
+  static constexpr auto state = Position{};
+  static constexpr auto scalar_substraction = displacement - state;
+  STATIC_REQUIRE(std::is_same_v<decltype(scalar_substraction)::depends_on, decltype(state)::depends_on>);
+}
+
+TEST_CASE("Operator Multiply With Scalar yields multiplication of operands", "[Operator]") 
+{
+  using namespace units::isq::si::references;
+  static constexpr auto scalar_number = 10.0;
+  static constexpr auto time = scalar_number * s;
+  static constexpr auto state = Velocity{};
+  static constexpr auto scalar_multiply = time * state;
+  static constexpr std::array values{2.0};
+  using TestSystem = codys::System<Velocity>;
+
+  static constexpr auto result = scalar_multiply.template evaluate<TestSystem, 1>(values);
+  STATIC_REQUIRE(result == scalar_number * values[0]);
+}
+
+TEST_CASE("Operator Multiply With Scalar is commutative", "[Operator]") 
+{
+  using namespace units::isq::si::references;
+  static constexpr auto scalar_number = 10.0;
+  static constexpr auto time = scalar_number * s;
+  static constexpr auto state = Velocity{};
+  static constexpr auto scalar_multiply1 = time * state;
+  static constexpr auto scalar_multiply2 = state * time;
+  static constexpr std::array values{2.0};
+  using TestSystem = codys::System<Velocity>;
+
+  static constexpr auto result1 = scalar_multiply1.template evaluate<TestSystem, 1>(values);
+  static constexpr auto result2 = scalar_multiply2.template evaluate<TestSystem, 1>(values);
+  STATIC_REQUIRE(result1 == result2);
+}
+
+TEST_CASE("Operator Multiply With Scalar has unit resulting from multiplying operands", "[Operator]")
+{
+  using namespace units::isq::si::references;
+  static constexpr auto time = 10 * s;
+  static constexpr auto state = Velocity{};
+  static constexpr auto scalar_multiply = time * state;
+  STATIC_REQUIRE(std::is_same_v<decltype(scalar_multiply)::Unit, PositionUnit>);
+}
+
+TEST_CASE("Operator Multiply With Scalar does not change dependencies", "[Operator]")
+{
+  using namespace units::isq::si::references;
+  static constexpr auto time = 10 * s;
+  static constexpr auto state = Velocity{};
+  static constexpr auto scalar_multiply = time * state;
+  STATIC_REQUIRE(std::is_same_v<decltype(scalar_multiply)::depends_on, decltype(state)::depends_on>);
+}
+
+TEST_CASE("Operator Divide by Scalar yields division of operands", "[Operator]") 
+{
+  using namespace units::isq::si::references;
+  static constexpr auto scalar_number = 10.0;
+  static constexpr auto time = scalar_number * s;
+  static constexpr auto state = Position{};
+  static constexpr auto scalar_division = state / time;
+  static constexpr std::array values{2.0};
+  using TestSystem = codys::System<Position>;
+
+  static constexpr auto result = scalar_division.template evaluate<TestSystem, 1>(values);
+  STATIC_REQUIRE(result == values[0] / scalar_number);
+}
+
+TEST_CASE("Operator Divide by Scalar yields unit from division", "[Operator]")
+{
+  using namespace units::isq::si::references;
+  static constexpr auto time = 10 * s;
+  static constexpr auto state = Position{};
+  static constexpr auto scalar_division = state / time;
+  STATIC_REQUIRE(std::is_same_v<decltype(scalar_division)::Unit, VelocityUnit>);
+}
+
+TEST_CASE("Operator Divide by Scalar does not change dependencies", "[Operator]")
+{
+  using namespace units::isq::si::references;
+  static constexpr auto time = 10 * s;
+  static constexpr auto state = Position{};
+  static constexpr auto scalar_division = state / time;
+  STATIC_REQUIRE(std::is_same_v<decltype(scalar_division)::depends_on, decltype(state)::depends_on>);
+}
+
+TEST_CASE("Operator Divide Scalar by State yields division of operands", "[Operator]") 
+{
+  using namespace units::isq::si::references;
+  static constexpr auto scalar_number = 10.0;
+  static constexpr auto ref_distance = scalar_number * m;
+  static constexpr auto state = Position{};
+  static constexpr auto scalar_division = ref_distance / state;
+  static constexpr std::array values{2.0};
+  using TestSystem = codys::System<Position>;
+
+  static constexpr auto result = scalar_division.template evaluate<TestSystem, 1>(values);
+  STATIC_REQUIRE(result == scalar_number / values[0]);
+}
+
+TEST_CASE("Operator Divide Scalar by State yields unit from division", "[Operator]")
+{
+  using namespace units::isq::si::references;
+  static constexpr auto ref_distance = 10.0 * m;
+  static constexpr auto state = Position{};
+  static constexpr auto scalar_division = ref_distance / state;
+  STATIC_REQUIRE(std::is_same_v<decltype(scalar_division)::Unit, Dimensionless>);
+}
+
+TEST_CASE("Operator Divide Scalar by State does not change dependencies", "[Operator]")
+{
+  using namespace units::isq::si::references;
+  static constexpr auto ref_distance = 10.0 * m;
+  static constexpr auto state = Position{};
+  static constexpr auto scalar_division = ref_distance / state;
+  STATIC_REQUIRE(std::is_same_v<decltype(scalar_division)::depends_on, decltype(state)::depends_on>);
 }
