1e7ab5e033f5edf5cf1cf751282d622d7a1116da
aber-code <aber.code.github@gmail.com>
2023-03-28 13:44:57 +0200

    Adapt project to new project cmake layout and add devcontainer

--- test/tests.cpp
+++ test/tests.cpp
@@ -15,22 +15,26 @@ using BasicMotions = codys::System<Position, Velocity>;
 using Acceleration = codys::State<class Acc_, units::isq::si::acceleration<units::isq::si::metre_per_second_sq>>;
 using BasicControls = codys::System<Acceleration>;
 
-struct StateSpace {
-    constexpr static auto make_dot() {
-        constexpr auto e1 = codys::dot<Velocity>(Acceleration{} + Acceleration{});
-        constexpr auto e2 = codys::dot<Position>(Velocity{});
-        return boost::hana::make_tuple(e1, e2);
-    }
+struct StateSpace
+{
+  constexpr static auto make_dot()
+  {
+    constexpr auto e1 = codys::dot<Velocity>(Acceleration{} + Acceleration{});
+    constexpr auto e2 = codys::dot<Position>(Velocity{});
+    return boost::hana::make_tuple(e1, e2);
+  }
 };
 
 TEST_CASE("StateSpaceSystem is evaluated correctly", "[StateSpace]")
 {
-  constexpr std::array statesIn{1.0, 2.0, 5.0};
+  constexpr std::array statesIn{ 1.0, 2.0, 5.0 };
 
-  std::array out{0.0, 0.0};
+  std::array out{ 0.0, 0.0 };
 
   codys::StateSpaceSystem<BasicMotions, BasicControls, StateSpace>::evaluate(statesIn, out);
 
-  REQUIRE(out[0] == Approx(2.0));
-  REQUIRE(out[1] == Approx(10.0));
+  constexpr auto expectedOutPutAt0 = 2.0;
+  constexpr auto expectedOutputAt1 = 10.0;
+  REQUIRE(out[0] == Approx(expectedOutPutAt0));
+  REQUIRE(out[1] == Approx(expectedOutputAt1));
 }
