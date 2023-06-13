78445575a9b2018aefbcf6d471174b9ee8cebbc5
hansepp <51990361+hansepp@users.noreply.github.com>
2023-06-13 13:45:58 +0200

    Add sin and cos to operator

--- test/tests.cpp
+++ test/tests.cpp
@@ -5,11 +5,13 @@
 #include <units/isq/si/acceleration.h>
 #include <units/isq/si/speed.h>
 #include <units/isq/si/length.h>
+#include <units/generic/angle.h>
 
 #include <codys/codys.hpp>
 
 using Position = codys::State<class Position_, units::isq::si::length<units::isq::si::metre>>;
 using Velocity = codys::State<class Vel_, units::isq::si::speed<units::isq::si::metre_per_second>>;
+using Rotation = codys::State<class Rot_, units::angle<units::radian, double>>;
 using BasicMotions = codys::System<Position, Velocity>;
 
 using Acceleration = codys::State<class Acc_, units::isq::si::acceleration<units::isq::si::metre_per_second_sq>>;
